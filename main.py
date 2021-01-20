bc89e33cdc6ad172f266484939cf808a381c5043
Shyzette <72762164+Shyzette@users.noreply.github.com>
2021-01-20 18:58:16 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
-    if input.sound_level() > 20 or input.temperature(TemperatureUnit.FAHRENHEIT) >105:
+    if input.sound_level() > 100 or input.temperature(TemperatureUnit.FAHRENHEIT) >105:
         light.set_all(light.rgb(255,0,0))
 
-    if input.sound_level() > 20 or input.temperature(TemperatureUnit.FAHRENHEIT) >105:
+    if input.sound_level() > 100 or input.temperature(TemperatureUnit.FAHRENHEIT) >105:
         music.pew_pew.play()
 
 
--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 while (true) {
-    if (input.soundLevel() > 20 || input.temperature(TemperatureUnit.Fahrenheit) > 105) {
+    if (input.soundLevel() > 100 || input.temperature(TemperatureUnit.Fahrenheit) > 105) {
         light.setAll(light.rgb(255, 0, 0))
     }
     
-    if (input.soundLevel() > 20 || input.temperature(TemperatureUnit.Fahrenheit) > 105) {
+    if (input.soundLevel() > 100 || input.temperature(TemperatureUnit.Fahrenheit) > 105) {
         music.pewPew.play()
     }
     
